d93b1e523363c5e478ba1ae4a0c373a25149f3cc
wangcheng <feiyan35488@gmail.com>
2017-12-12 09:49:55 +0800

    调整url列表，方便管理

--- src/main/java/net/helao/novel/books/task/NovelTask.kt
+++ src/main/java/net/helao/novel/books/task/NovelTask.kt
@@ -2,6 +2,7 @@ package net.helao.novel.books.task
 
 import freemarker.template.Configuration
 import org.apache.commons.io.FileUtils
+import org.apache.commons.lang3.StringUtils
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.scheduling.annotation.Scheduled
@@ -22,7 +23,9 @@ class NovelTask constructor(@Value("\${base.dir}") private val baseDir: String,
     fun run() {
         try {
             logger.info("开始抓取")
-            val urls = FileUtils.readLines(File(urlsFile), StandardCharsets.UTF_8)
+            val urls = FileUtils.readLines(File(urlsFile), StandardCharsets.UTF_8).filter {
+                StringUtils.isNotBlank(it) && !StringUtils.startsWith(it,"#")
+            }
             val novel = Novel(urls, baseDir, conf)
             novel.fetch()
             logger.info("抓取完成")
